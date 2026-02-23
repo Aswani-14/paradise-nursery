91082858ed7c56bda2b64595b3a6785f1faaf003
Aswani K B <aswanikb14@gmail.com>
2026-02-23 23:28:21 +0530

    Add AboutUs component for company information

---
+++ AboutUs.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function AboutUs() {
+  return (
+    <div className="about-us">
+      <h2>About Paradise Nursery</h2>
+      <p>
+        Paradise Nursery is dedicated to bringing greenery into your home and office. 
+        We offer a wide variety of houseplants that are carefully selected for quality 
+        and ease of care. Our mission is to make plant shopping simple, enjoyable, and 
+        accessible to everyone.
+      </p>
+      <p>
+        Our company focuses on customer satisfaction, sustainable sourcing, and creating 
+        a community of plant lovers who share tips, care guides, and inspiration.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
